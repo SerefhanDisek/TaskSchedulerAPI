9db72444ee0579f59f01f86f907dc6ba409ecadc
seref <seref@DESKTOP-9S2EU5A>
2024-08-26 11:09:47 +0300

    Services

---
+++ TaskSchedulerAPI.DataAccess/Repositories/TaskRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskSchedulerAPI.Core.Entities;
+using TaskSchedulerAPI.Core.Interfaces;
+
+namespace TaskSchedulerAPI.DataAccess.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly TaskSchedulerDbContext _context;
+
+        public TaskRepository(TaskSchedulerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Task>> GetAllAsync()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+
+        public async Task<Task> GetByIdAsync(int id)
+        {
+            return await _context.Tasks.FindAsync(id);
+        }
+
+        public async Task AddAsync(Task task)
+        {
+            await _context.Tasks.AddAsync(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Task task)
+        {
+            _context.Tasks.Update(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Task task)
+        {
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ TaskSchedulerAPI.DataAccess/Repositories/UserTaskRepository.cs
@@ -0,0 +1,21 @@
+﻿using TaskSchedulerAPI.Core.Entities;
+using TaskSchedulerAPI.Core.Interfaces;
+
+namespace TaskSchedulerAPI.DataAccess.Repositories
+{
+    public class UserTaskRepository : IUserTaskRepository
+    {
+        private readonly TaskSchedulerDbContext _context;
+
+        public UserTaskRepository(TaskSchedulerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(UserTask userTask)
+        {
+            await _context.UserTasks.AddAsync(userTask);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
