2b3a2e739e5e3fa69868b5c2378bcbabfe16345d
seref <seref@DESKTOP-9S2EU5A>
2024-08-26 11:49:35 +0300

    AutoMapper

---
+++ TaskSchedulerAPI.Business/Services/TaskService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskSchedulerAPI.Core.DTOs;
+using TaskSchedulerAPI.Core.Interfaces.Services;
+
+namespace TaskSchedulerAPI.Business.Services
+{
+    public class TaskService : ITaskService
+    {
+        public Task<TaskDto> CreateTaskAsync(TaskCreateDto taskDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteTaskAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TaskDto>> GetAllTasksAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskDto> GetTaskByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateTaskAsync(int id, TaskUpdateDto taskDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TaskSchedulerAPI.Presentation/Program.cs
+++ TaskSchedulerAPI.Presentation/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using TaskSchedulerAPI.Core.Interfaces;
+using TaskSchedulerAPI.Core.Interfaces.Repositories;
+using TaskSchedulerAPI.Core.Interfaces.Services;
 using TaskSchedulerAPI.DataAccess;
+using TaskSchedulerAPI.DataAccess.Repositories;
+using TaskSchedulerAPI.Business.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +14,14 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<TaskSchedulerDbContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITaskService, TaskService>();
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+
+builder.Services.AddAutoMapper(typeof(TaskScheduler));
 
 var app = builder.Build();
 
