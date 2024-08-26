2b543f639232f6f0813dfb864889648833bb6eb9
seref <seref@DESKTOP-9S2EU5A>
2024-08-26 15:06:43 +0300

    MappingProfile

--- TaskSchedulerAPI.Core/MappingProfiles/UserProfile.cs
+++ TaskSchedulerAPI.Core/MappingProfiles/UserProfile.cs
@@ -8,8 +8,36 @@ namespace TaskSchedulerAPI.Core.MappingProfiles
     {
         public UserProfile()
         {
+            //User
             CreateMap<User, UserDto>();
             CreateMap<UserDto, User>();  
+
+            CreateMap<User, UserCreateDto>();
+            CreateMap<UserCreateDto, User>();
+
+            CreateMap<User, UserUpdateDto>();
+            CreateMap<UserUpdateDto, User>();
+
+            CreateMap<User, UserLoginDto>();
+            CreateMap<UserLoginDto, User>();
+
+            //Task
+            CreateMap<Tasks, TaskDto>();
+            CreateMap<TaskDto, Tasks>();
+
+            CreateMap<Tasks, TaskCreateDto>();
+            CreateMap<TaskCreateDto, Tasks>();
+
+            CreateMap<Tasks, TaskUpdateDto>();
+            CreateMap<TaskUpdateDto, Tasks>();
+
+            //Log
+            CreateMap<Log, LogDto>();
+            CreateMap<LogDto, Log>();
+
+            //SystemSettings
+            CreateMap<SystemSettings, UpdateSystemSettingsDto>();
+            CreateMap<UpdateSystemSettingsDto, SystemSettings>();
         }
     }
 }
