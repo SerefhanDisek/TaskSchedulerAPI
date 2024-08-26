262a7989e2efde44dc62efa3f32eed9553e5c5db
seref <seref@DESKTOP-9S2EU5A>
2024-08-26 11:37:50 +0300

    UserController

---
+++ TaskSchedulerAPI.Presentation/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TaskSchedulerAPI.Core.DTOs;
+using TaskSchedulerAPI.Core.Interfaces.Services;
+
+namespace TaskSchedulerAPI.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _userService.GetAllUsersAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var user = await _userService.GetUserByIdAsync(id);
+            if (user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(UserCreateDto userDto)
+        {
+            var user = await _userService.CreateUserAsync(userDto);
+            return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserUpdateDto userDto)
+        {
+            var result = await _userService.UpdateUserAsync(id, userDto);
+            if (!result)
+                return NotFound();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var result = await _userService.DeleteUserAsync(id);
+            if (!result)
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
