438e96af21b667fa35315b43ab2462b54f469808
SerefhanDisek <glorius488@gmail.com>
2024-08-27 17:17:04 +0300

    TaskDistributionController eklendi

---
+++ TaskSchedulerAPI.Presentation/Controllers/TaskDistributionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TaskSchedulerAPI.Core.Interfaces;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class TaskDistributionController : ControllerBase
+{
+    private readonly ITaskDistributionService _taskDistributionService;
+
+    public TaskDistributionController(ITaskDistributionService taskDistributionService)
+    {
+        _taskDistributionService = taskDistributionService;
+    }
+
+    [HttpPost("distribute")]
+    public async Task<IActionResult> DistributeTasks()
+    {
+        await _taskDistributionService.DistributeTasksAsync();
+        return Ok("Tasks have been distributed.");
+    }
+}
+
