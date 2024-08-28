655603106c2786461a46fd23b48b1f514aa728ed
SerefhanDisek <glorius488@gmail.com>
2024-08-28 11:14:08 +0300

    TasksController

--- TaskSchedulerAPI.Presentation/Controllers/TasksController.cs
+++ TaskSchedulerAPI.Presentation/Controllers/TasksController.cs
@@ -1,23 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TaskSchedulerAPI.Business.Services;
+using TaskSchedulerAPI.Core.DTOs;
 using TaskSchedulerAPI.Core.Interfaces.Services;
 using TaskSchedulerAPI.DataAccess;
 
 namespace TaskSchedulerAPI.Presentation.Controllers
 {
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
     public class TasksController : Controller
     {
-        private readonly ITaskService taskService;
-        private readonly TaskSchedulerDbContext context;
+        private readonly ITaskService _taskService;
+        private readonly TaskSchedulerDbContext _context;
 
         public TasksController(ITaskService taskService, TaskSchedulerDbContext context)
         {
-            this.taskService = taskService;
-            this.context = context;
+            _taskService = taskService;
+            _context = context;
         }
 
-        public IActionResult Index()
+        [HttpPost]
+        public async Task<IActionResult> CreateTask([FromBody] TaskCreateDto taskDto)
         {
-            return View();
+            var task = await _taskService.CreateTaskAsync(taskDto);
+            return Ok(task);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTasks()
+        {
+            var tasks = await _taskService.GetAllTasksAsync();
+            return Ok(tasks);
         }
     }
 }
