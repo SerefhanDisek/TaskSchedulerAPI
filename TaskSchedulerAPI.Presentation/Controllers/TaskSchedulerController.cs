927775d17ed4f6c99ada8bc5126bbd2630aa6bc2
SerefhanDisek <glorius488@gmail.com>
2024-10-22 16:43:03 +0300

    TaskScheduler

---
+++ TaskSchedulerAPI.Presentation/Controllers/TaskSchedulerController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskSchedulerAPI.Core.Interfaces;
+
+namespace TaskSchedulerAPI.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskSchedulerController : ControllerBase
+
+    {
+        private readonly ITaskSchedulerService _taskSchedulerService;
+
+        public TaskSchedulerController(ITaskSchedulerService taskSchedulerService) 
+        {
+            _taskSchedulerService = taskSchedulerService;
+        }
+
+        [HttpPost("assign")]
+        public async Task<IActionResult> AssignTaskToUser(int taskId, int userId)
+        {
+            var result = await _taskSchedulerService.AssignTaskToUserAsync(taskId, userId);
+            if (!result)
+            {
+                return BadRequest("Task could not be assigned to user.");
+            }
+
+            return Ok("Task assigned to user successfully.");
+        }
+    }
+}
